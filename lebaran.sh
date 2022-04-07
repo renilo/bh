3a407f031d0c176c9b780532abe1e4ed7382f5b5
renilo <88342640+renilo@users.noreply.github.com>
2022-04-07 22:18:44 +0700

    Update lebaran.sh

--- lebaran.sh
+++ lebaran.sh
@@ -11,7 +11,7 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 45.9.154.251:12324
+socks5 = 192.252.209.155:14455
 socks5_username = 14a5029e51675
 socks5_password = 9d332fa7c0
 END
