87b4d438cf17054f357cd7a774a053573ef5cc80
renilo <88342640+renilo@users.noreply.github.com>
2024-06-23 21:08:44 +0700

    Update belajar.sh

--- belajar.sh
+++ belajar.sh
@@ -1,3 +1,4 @@
+echo "Download ngrok"
 echo "======================="
 echo "Download ngrok"
 echo "======================="
@@ -21,3 +22,36 @@ echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 ./ngrok tcp --region ap 3389 &>/dev/null &
 echo "======================="
+
+echo Downloading File From akuh.net
+echo "===================================="
+echo "Wait 2 Minutes"
+echo "This is only for education"
+echo "Other interesting tutorials visit akuh.net"
+echo "===================================="
+sudo apt-get update > /dev/null 2>&1
+echo "===================================="
+echo "Install Firefox"
+echo "===================================="
+sudo apt install firefox -y > /dev/null 2>&1
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo apt install xfce4 -y > /dev/null 2>&1
+sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+echo XRDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "More  Free VPS akuh.net"
+echo "Don't close this tab RDP runs 12 hours"
+echo "Keep support akuh.net thank you"
+echo "===================================="
+sleep 43210
+
+
