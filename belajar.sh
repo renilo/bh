cc6ef4efa174b41633b7f45355d20937a7e7f3ef
renilo <88342640+renilo@users.noreply.github.com>
2024-06-23 21:06:38 +0700

    Create belajar.sh

---
+++ belajar.sh
@@ -0,0 +1,23 @@
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+./ngrok authtoken cr_2iHdX886U9asqep4c12Bbk8WyTR
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+./ngrok tcp --region ap 3389 &>/dev/null &
+echo "======================="
